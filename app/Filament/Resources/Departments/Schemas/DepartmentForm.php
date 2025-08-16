78f43e39c7f9b5769b6e5d453ea4380e5b86e044
titustum <tituskiptanuitum@gmail.com>
2025-08-16 21:13:02 +0300

    Feat: added departments filament resource

---
+++ app/Filament/Resources/Departments/Schemas/DepartmentForm.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources\Departments\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\FileUpload;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Str;
+
+class DepartmentForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->schema([
+                Section::make('Department Info')
+                    ->columns(2)
+                    ->schema([
+                        TextInput::make('name')
+                            ->required()
+                            ->maxLength(255)
+                            ->live(debounce: 500)
+                            ->afterStateUpdated(function ($state, callable $set) {
+                                $set('slug', Str::slug($state));
+                            }),
+
+                        TextInput::make('slug')
+                            ->required()
+                            ->unique(ignoreRecord: true)
+                            ->maxLength(255),
+
+                        TextInput::make('short_desc')
+                            ->label('Short Description')
+                            ->required()
+                            ->maxLength(255),
+                    ]),
+
+                Section::make('Media')
+                    ->columns(2)
+                    ->schema([
+                        FileUpload::make('photo')
+                            ->label('Department Photo')
+                            ->image()
+                            ->imageEditor()
+                            ->directory('departments/photos')
+                            ->required(),
+
+                        FileUpload::make('banner_pic')
+                            ->label('Banner Picture')
+                            ->image()
+                            ->imageEditor()
+                            ->directory('departments/banners')
+                            ->required(),
+                    ]),
+
+                Section::make('Full Description')
+                    ->schema([
+                        Textarea::make('full_desc')
+                            ->label('Detailed Description')
+                            ->rows(8)
+                            ->required(),
+                    ]),
+            ]);
+    }
+}
